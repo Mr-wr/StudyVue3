acac4708e20455a2e803bdd7e87018626a15ca2d
xie <1434687824@qq.com>
2024-01-03 21:24:11 +0800

    h5初始化

---
+++ vue3_dome07/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TestH5 from "@/components/H5/TestH5.vue";
+</script>
+
+<template>
+  <TestH5></TestH5>
+</template>
+
+<style scoped></style>
---
+++ vue3_dome07/src/components/H5/TestH5.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: xie 1434687824@qq.com
+ * @Date: 2024-01-03 21:04:11
+ * @LastEditors: xie 1434687824@qq.com
+ * @LastEditTime: 2024-01-03 21:04:20
+ * @FilePath: \vue3_dome07\src\components\H5\TestH5.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <div class='H5'>
+    </div>
+</template>
+<script lang='ts'>
+export default {
+    //eslint-disable-next-line
+    name: 'H5',
+}
+</script>
+<script  setup lang='ts'>
+</script>
+<style lang='scss' scoped></style>
