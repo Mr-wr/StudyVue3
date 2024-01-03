fddee2f795e595a5734d3ba18148d0a674a2d46d
xie <1434687824@qq.com>
2024-01-03 17:40:38 +0800

    安装tailwind css

--- vue3_dome06/src/App.vue
+++ vue3_dome06/src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: xie 1434687824@qq.com
+ * @Date: 2024-01-03 16:46:47
+ * @LastEditors: xie 1434687824@qq.com
+ * @LastEditTime: 2024-01-03 17:38:41
+ * @FilePath: \vue3_dome06\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
@@ -5,19 +13,22 @@ let testa = ref(0)
 </script>
 
 <template>
-  <header>
-    {{ testa }}
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <el-button type="primary">afds</el-button>
-    <el-text class="mx-1" type="success">Warning</el-text>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="max-w-md text-ellipsis  mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div class=""></div>
+    <div class="md:flex">
+      <div class="md:flex-shrink-0">
+        <img class="h-48 w-full object-cover md:w-48" src="http://n.sinaimg.cn/translate/20170815/OoVn-fyixtym5144510.jpg"
+          alt="Man looking at item at a store">
+      </div>
+      <div class="p-8">
+        <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+        <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers
+          for your new business</a>
+        <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas
+          you can use to find your first customers.</p>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
---
+++ vue3_dome06/tailwind.config.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: xie 1434687824@qq.com
+ * @Date: 2024-01-03 17:35:58
+ * @LastEditors: xie 1434687824@qq.com
+ * @LastEditTime: 2024-01-03 17:36:38
+ * @FilePath: \vue3_dome06\tailwind.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
