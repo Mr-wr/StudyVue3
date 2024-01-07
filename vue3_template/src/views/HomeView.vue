11f09c592de2c2c67e66d32bca2d32a16e22f002
xie <1434687824@qq.com>
2024-01-07 10:51:42 +0800

    初始化

---
+++ vue3_template/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='home'>
+    <h1>home</h1>
+  </div>
+</template>
+<script lang='ts'>
+export default {
+  //eslint-disable-next-line
+  name: 'Home',
+}
+</script>
+<script  setup lang='ts'>
+</script>
+<style lang='scss' scoped>
+.home {
+  h1 {
+    color: red;
+  }
+}
+</style>
