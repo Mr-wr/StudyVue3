ffab5d28c1fa55b8b9ccb85c34ab9484029db6bb
xie <1434687824@qq.com>
2024-01-07 22:52:34 +0800

    安装postcss-px-to-viewport和在config文件中使用

---
+++ vue3_template/src/components/Menu.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class='menu'>
+
+    </div>
+</template>
+<script lang='ts'>
+export default {
+    //eslint-disable-next-line
+    name: 'Menu',
+}
+</script>
+<script  setup lang='ts'>
+</script>
+<style lang='scss' scoped></style>
